adc30d174e2f3d8f66c913d5e0c8c8a3228bd5b9
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-04-14 00:19:42 +0800

    default home page

--- views/home.php
+++ views/home.php
@@ -7,10 +7,11 @@
 		public string $message = '';
 		public array $logs = [];
 
-		public function submit($message): void
+		public function submit(): void
 		{
-			$this->message = $message;
-			$this->logs[] = $message;
+			if ($this->message) {
+				$this->logs[] = $this->message;
+			}
 		}
 
 		public function render(): string
@@ -21,16 +22,22 @@
 
 			return <<<HTML
 			    <div class="flex flex-col gap-3 p-5 border border-gray-200">
-			        $label
-			        <input type="text" placeholder="Type here..." wire:keydown.enter="submit(event.target.value)" class="input">
+			       <div class="flex flex-row gap-5 items-center"> 
+			       		<input type="text" class="input" placeholder="Type here..." 
+			       			wire:model="message" 
+			       			wire:keydown.enter.clear.prevent="submit" />
+			       		<input type="button" value="Click Me" class="btn" 
+			       			wire:click.prevent="submit" />
+			        	$label
+			       </div>
 			        <ul class="bg-base-100 rounded-sm shadow-md border border-gray-600"> 
 			        	<li class="p-4 text-xs opacity-60 tracking-wide border-b border-gray-500">Submitted Logs:</li>
 			        	{$this->print(function() {
 							if ($this->logs) {
 								
 								echo '<li class="border-t border-gray-500 p-0"></li>';
-
 								$lists = implode("</li><li class='px-4 py-3 text-sm'>", $this->logs);
+								
 								echo <<<HTML
 								  <li class="px-4 py-3 text-sm">
 									$lists
