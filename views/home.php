7484df846150c08d8207455b48d8e1a82ddd4502
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-04-14 23:24:10 +0800

    add loader

--- views/home.php
+++ views/home.php
@@ -16,19 +16,26 @@
 
 		public function render(): string
 		{
-			$label = '';
-			if ($this->message)
-				$label = "<label>You Entered: <b>{$this->message}</b></label>";
+			$hidden = empty(trim($this->message)) ? 'display-none' : '';
 
 			return <<<HTML
 			    <div class="flex flex-col gap-3 p-5 border border-gray-200">
 			       <div class="flex flex-row gap-5 items-center"> 
 			       		<input type="text" class="input" placeholder="Type here..." 
 			       			wire:model="message" 
-			       			wire:keydown.enter.clear.prevent="submit" />
+			       			wire:keydown.enter.clear.prevent="submit" 
+			       			wire:loader.attr="disabled" />
 			       		<input type="button" value="Click Me" class="btn" 
-			       			wire:click.prevent="submit" />
-			        	$label
+			       			wire:click.prevent="submit"  
+			       			wire:loader.attr="disabled" />
+			       		<label class='progress-info hidden' 
+							wire:loader.classList.remove='hidden'>
+							Loading...
+						</label>
+			        	<label class="$hidden"
+			        		wire:loader.classList.add='hidden'>
+			        		You Entered: <b>{$this->message}</b>
+			        	</label>
 			       </div>
 			        <ul class="bg-base-100 rounded-sm shadow-md border border-gray-600"> 
 			        	<li class="p-4 text-xs opacity-60 tracking-wide border-b border-gray-500">Submitted Logs:</li>
