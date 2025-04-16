64854d65a18c732bfbafd19def0bd28d1d35a4db
ROBROY BUSTILLO CANALES <canales.robroy123@gmail.com>
2025-04-16 17:29:58 +0800

    new function added for templates

--- app/Standard.php
+++ app/Standard.php
@@ -1,5 +1,7 @@
 <?php
 
+	use App\Content\Blade;
+
 	function dump(mixed $data, bool $exit = false): void
 	{
 		$printed = print_r($data, true);
@@ -10,4 +12,25 @@
 		HTML;
 
 		if ($exit) exit;
+	}
+
+	function view(string $path, array $data = []): string
+	{
+		ob_start();
+
+		$root = rtrim(config('APP_ROOT'), '/') . '/views/';
+		$normalizedPath = trim($path, '/');
+		$normalizedPath = preg_replace('/\.php$/', '', $normalizedPath);
+		$fullPath = $root . $normalizedPath . '.php';
+
+		if (file_exists($temp = str_replace('.php', '.blade.php', $fullPath)))
+			$fullPath = $temp;
+
+		if (file_exists($fullPath)) {
+
+			$compiled = Blade::compile(file_get_contents($fullPath));
+			Blade::eval($compiled, $data);
+		}
+
+		return ob_get_clean();
 	}
